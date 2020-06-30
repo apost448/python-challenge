c768a9a43e4c7e2a67695a30793f2c2259da05f5
apost448 <65988285+apost448@users.noreply.github.com>
2020-06-29 22:15:24 -0500

    Add files via upload

---
+++ PyBank/main.py
@@ -0,0 +1,71 @@
+# Dependencies
+import csv
+import os
+
+# Files to load and output (Remember to change these)
+file_to_load = os.path.join("C:\\Users\\16158\\Desktop\\PyBank\\resources.csv")
+file_to_output = os.path.join("C:\\Users\\16158\\Desktop\\PyBank\\analysis")
+
+# Track various financial parameters
+total_months = 0
+month_of_change = []
+net_change_list = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999]
+total_net = 0
+
+# Read csv and convert into a list of dictionaries
+with open(file_to_load) as financial_data:
+    reader = csv.reader(financial_data)
+
+    # Read header row
+    header = next(reader)
+
+    # Extract first row to avoid appending to net_change_list
+    first_row = next(reader)
+    total_months += 1
+    total_net += int(first_row[1])
+    prev_net = int(first_row[1])
+
+    for row in reader:
+
+        # Track total
+        total_months += 1
+        total_net += int(row[1])
+
+        # Track net change
+        net_change = int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list += [net_change]
+        month_of_change += [row[0]]
+
+        # Calculate greatest increase
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+
+        # Calculate greatest decrease
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+
+# Calculate Average Net Change
+net_monthly_avg = sum(net_change_list) / len(net_change_list)
+
+# Generate Output Summary
+output = (
+    f"Financial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_net}\n"
+    f"Average  Change: ${net_monthly_avg:.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+# Print output (to terminal)
+print(output)
+
+# Export results to text file
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(output)
+
